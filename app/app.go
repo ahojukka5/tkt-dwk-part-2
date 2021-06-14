fd2d484fc738d990c04044a6f8fe1d87ba400a00
Jukka Aho <ahojukka5@gmail.com>
2021-06-14 23:35:30 +0300

    Return something for GET request

--- app/app.go
+++ app/app.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, world!")
+	})
+
 	port := "8000"
 	println("Server started in port " + port)
 	http.ListenAndServe(":"+port, nil)

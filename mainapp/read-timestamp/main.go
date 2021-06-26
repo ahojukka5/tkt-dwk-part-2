95f24f7e8c10d7e8cf5c2292f24919800bf6ab4e
Jukka Aho <ahojukka5@gmail.com>
2021-06-26 14:43:49 +0300

    Use ConfigMap to configure mainapp

--- mainapp/read-timestamp/main.go
+++ mainapp/read-timestamp/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	uuid "github.com/satori/go.uuid"
 )
@@ -12,12 +13,14 @@ const port = ":3000"
 
 func main() {
 	random_string := uuid.NewV4()
+	welcome_message := os.Getenv("MESSAGE")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		t, err := ioutil.ReadFile("/cache/timestamp")
 		if err != nil {
 			panic(err)
 		}
+		fmt.Fprintln(w, welcome_message)
 		fmt.Fprintf(w, "%s: %s.\n", t, random_string)
 
 		// for local testing

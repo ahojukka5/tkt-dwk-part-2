07652a466b4a0fc7410f55887419d0ed2f5f52b7
Jukka Aho <ahojukka5@gmail.com>
2021-06-26 12:03:16 +0300

    Add debug print to pingpong

--- pingpong/main.go
+++ pingpong/main.go
@@ -6,17 +6,19 @@ import (
 	"net/http"
 )
 
+const port = ":8000"
+
 func main() {
 	cnt := 0
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		println("pingpong called, cnt =", cnt)
 		fmt.Fprint(w, cnt)
 		cnt += 1
 	})
 
-	port := "8000"
-	println("Ping/pong server started in port " + port)
-	err := http.ListenAndServe(":"+port, nil)
+	println("Ping/pong server listening in address http://localhost" + port)
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}

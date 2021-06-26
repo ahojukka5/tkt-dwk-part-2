cabe2adeee9c27e75529ab89f41654044028ea79
Jukka Aho <ahojukka5@gmail.com>
2021-06-26 20:36:32 +0300

    Use mongodb to store todos

--- todo-backend/main.go
+++ todo-backend/main.go
@@ -1,30 +1,62 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Item struct {
-	ID   int    `json:"id"`
+const port = ":8000"
+
+var ctx = context.TODO()
+var collection = ConnectDB()
+
+type ItemWithoutID struct {
 	Task string `json:"task"`
 }
 
-var db = []Item{
-	{
-		ID:   1,
-		Task: "Buy coffee",
-	},
-	{
-		ID:   2,
-		Task: "Drink coffee",
-	},
+type Item struct {
+	ID   primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	Task string             `json:"task"`
 }
 
-var curId = 2
+func Getenv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func getConnectionURI() string {
+	username := Getenv("MONGO_USERNAME", "root")
+	password := Getenv("MONGO_PASSWORD", "")
+	host := Getenv("MONGO_HOST", "todo-database-svc")
+	mongo_uri := "mongodb://" + username + ":" + password + "@" + host
+	return mongo_uri
+}
+
+func ConnectDB() *mongo.Collection {
+	println(getConnectionURI())
+	clientOptions := options.Client().ApplyURI(getConnectionURI())
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+	db := client.Database("todo")
+	col := db.Collection("items")
+	return col
+}
 
 func Health(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -32,34 +64,57 @@ func Health(w http.ResponseWriter, r *http.Request) {
 }
 
 func getTodos(w http.ResponseWriter, r *http.Request) {
-	println("todo-backend: getTodos")
+	println("getTodos")
 	w.Header().Set("Content-Type", "application/json")
-	db, err := json.Marshal(db)
+	var items []Item
+	println("getTodos: fetching collection")
+	cur, err := collection.Find(ctx, bson.M{})
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
-	io.WriteString(w, string(db))
+	defer cur.Close(ctx)
+	println("getTodos: looping collection")
+	for cur.Next(ctx) {
+		var item Item
+		err := cur.Decode(&item)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		items = append(items, item)
+	}
+	println("getTodos: encoding to json")
+	err = json.NewEncoder(w).Encode(items)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 func postTodo(w http.ResponseWriter, r *http.Request) {
-	println("todo-backend: postTodo")
-	decoder := json.NewDecoder(r.Body)
+	println("postTodo")
+	w.Header().Set("Content-Type", "application/json")
 	var item Item
-	err := decoder.Decode(&item)
+	var itemWithoutID ItemWithoutID
+	err := json.NewDecoder(r.Body).Decode(&itemWithoutID)
 	if err != nil {
-		panic(err)
+		fmt.Println("Failed to parse input:", err)
+		return
 	}
-
-	println("New todo item: " + item.Task)
-	curId += 1
-	item.ID = curId
-	db = append(db, item)
-
-	resp, err := json.Marshal(item)
+	item.ID = primitive.NewObjectID()
+	item.Task = itemWithoutID.Task
+	println("postTodo: new todo item: " + item.Task)
+	result, err := collection.InsertOne(ctx, item)
 	if err != nil {
-		panic(err)
+		fmt.Println("Failed to insert to database:", err)
+		return
 	}
-	io.WriteString(w, string(resp))
+	err = json.NewEncoder(w).Encode(result)
+	if err != nil {
+		fmt.Println("Failed to encode to json:", err)
+		return
+	}
 }
 
 func main() {
@@ -67,6 +122,6 @@ func main() {
 	router.HandleFunc("/health", Health).Methods("GET")
 	router.HandleFunc("/todos", getTodos).Methods("GET")
 	router.HandleFunc("/todos", postTodo).Methods("POST")
-	println("todo-backend listening on port 8000")
-	http.ListenAndServe(":8000", router)
+	println("Server listening in address http://localhost" + port)
+	http.ListenAndServe(port, router)
 }
